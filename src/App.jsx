10f42f0f0aee7ec3f10a71b2751d88a72c57841c
unknown <intemeretor@gmail.com>
2023-09-23 20:44:56 +0300

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import './App.scss'
 
-import Head from '../components/Head/Head';
-import Todo from '../components/Todo/Todo';
-import Weeks from '../components/Weeks/Weeks';
+import Head from './components/Head/Head';
+import Todo from './components/Todo/Todo';
+import Weeks from './components/Weeks/Weeks';
 function App() {
 
 	return (
