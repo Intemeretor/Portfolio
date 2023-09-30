9481f22ec0a0c35d79c2ffd1b26779abd594edc9
unknown <intemeretor@gmail.com>
2023-09-30 21:00:55 +0300

    yeah

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.scss'
 
-import Head from './components/Header/Header';
+import Head from './components/Head/Head';
 import Todo from './components/Todo/Todo';
 import Weeks from './components/Weeks/Weeks';
 
--- src/components/Head/Head.jsx
+++ src/components/Head/Head.jsx
@@ -1,4 +1,4 @@
-import './Header.scss';
+import './Head.scss';
 import { useState } from 'react';
 
 export default function Head() {
